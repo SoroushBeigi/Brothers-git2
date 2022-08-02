dc37560217090563c59d253d8732c312bf34e771
a m <aballs210@gmail.com>
2022-08-02 16:08:18 +0430

    add Mohammad Ali to code

--- code/participants.c
+++ code/participants.c
@@ -34,6 +34,7 @@ main (int argc, char *argv[])
 
   printf ("%02d: Eve Lyons-Berg\n", ++i);
   printf ("%02d: amin shenabi\n", ++i);
+  printf ("%02d: Mohammad Ali\n", ++i);
   printf ("There are %i participants.\n", i);
 
   return 0;
