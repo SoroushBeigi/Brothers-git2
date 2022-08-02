0de44d0d36646ee5d4bb7c03aed43142a7749fda
Diyako97 <diakowork@gmail.com>
2022-08-02 16:13:01 +0430

    adding diyako to code

--- code/participants.c
+++ code/participants.c
@@ -35,6 +35,7 @@ main (int argc, char *argv[])
   printf ("%02d: Eve Lyons-Berg\n", ++i);
   printf ("%02d: amin shenabi\n", ++i);
   printf ("%02d: Mohammad Ali\n", ++i);
+  printf ("%02d: Diyako Hajjari\n", ++i);
   printf ("There are %i participants.\n", i);
 
   return 0;
